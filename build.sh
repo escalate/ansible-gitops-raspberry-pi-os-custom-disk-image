e2c6004ab226cff5ccadab12e8918735881291e2
Felix Boerner <ich@felix-boerner.de>
2021-02-07 10:23:41 +0100

    feat: improve build script

--- build.sh
+++ build.sh
@@ -2,26 +2,26 @@
 set -eo pipefail
 
 DOWNLOAD_DIR="$(curl --silent 'https://downloads.raspberrypi.org/raspios_lite_armhf/images/?C=M;O=D' | grep --extended-regexp --only-matching 'raspios_lite_armhf-[0-9]{4}-[0-9]{2}-[0-9]{2}' | head -n 1)"
-DOWNLOAD_ZIP_FILE="$(curl --silent https://downloads.raspberrypi.org/raspios_lite_armhf/images/${DOWNLOAD_DIR}/ | grep --extended-regexp --only-matching '[0-9]{4}-[0-9]{2}-[0-9]{2}-raspios-buster-armhf-lite\.zip' | head -n 1)"
+DOWNLOAD_ZIP_FILE="$(curl --silent "https://downloads.raspberrypi.org/raspios_lite_armhf/images/${DOWNLOAD_DIR}/" | grep --extended-regexp --only-matching '[0-9]{4}-[0-9]{2}-[0-9]{2}-raspios-buster-armhf-lite\.zip' | head -n 1)"
 DOWNLOAD_FILENAME="${DOWNLOAD_ZIP_FILE%%.*}"
 
-echo "Download latest image archive and checksum"
-wget --no-verbose "https://downloads.raspberrypi.org/raspios_lite_armhf/images/${DOWNLOAD_DIR}/${DOWNLOAD_ZIP_FILE}"
-wget --no-verbose "https://downloads.raspberrypi.org/raspios_lite_armhf/images/${DOWNLOAD_DIR}/${DOWNLOAD_ZIP_FILE}.sha256"
+echo "Download latest image archive"
+wget --no-verbose --no-clobber "https://downloads.raspberrypi.org/raspios_lite_armhf/images/${DOWNLOAD_DIR}/${DOWNLOAD_ZIP_FILE}"
 
 echo "Verify downloaded image archive"
-sha256sum --check ${DOWNLOAD_ZIP_FILE}.sha256
+wget --no-verbose --no-clobber "https://downloads.raspberrypi.org/raspios_lite_armhf/images/${DOWNLOAD_DIR}/${DOWNLOAD_ZIP_FILE}.sha256"
+sha256sum --check "${DOWNLOAD_ZIP_FILE}.sha256"
 
 echo "Unarchive zip file"
-unzip -q ${DOWNLOAD_ZIP_FILE}
+unzip -qfo "${DOWNLOAD_ZIP_FILE}"
 
 echo "Set up loop devices"
-LOOP_DEVICE="$(sudo losetup --find --show --partscan ${DOWNLOAD_FILENAME}.img)"
+LOOP_DEVICE="$(sudo losetup --find --show --partscan "${DOWNLOAD_FILENAME}.img")"
 
 echo "Mount loop devices"
-mkdir boot
+mkdir --parents boot
 sudo mount "${LOOP_DEVICE}p1" boot
-mkdir rootfs
+mkdir --parents rootfs
 sudo mount "${LOOP_DEVICE}p2" rootfs
 
 echo "Customize image"
@@ -31,6 +31,8 @@ echo "Flush write cache"
 sync
 
 echo "Umount loop devices"
+# Wait 5 secs to get rid of "target is busy" error
+sleep 5
 sudo umount boot
 sudo umount rootfs
 
@@ -44,4 +46,4 @@ echo "Hash custom image archive"
 sha256sum "${DOWNLOAD_FILENAME}-custom.tar.bz2" > "${DOWNLOAD_FILENAME}-custom.tar.bz2.sha256"
 
 echo "Show final artifacts"
-ls -l *-custom.tar.bz2*
+ls -l ./*-custom.tar.bz2*
